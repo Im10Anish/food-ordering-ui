c82bdd0248ff834289b22880dec4d6f436feff95
anish kumar <anish.kumar@dibizglobal.com>
2021-11-29 14:12:20 +0530

    completed header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,26 @@ const Header = () => {
     return (
         <div className="header">
             <div className="container">
-                <div className="left"> left</div>
-                <div className="right"> right</div>
+                <div className="left">
+                    <div>Icon</div>
+                    <button className="btn">Brooklyn, NY</button>
+                </div>
+                <div className="right">
+                    <div className="right-column-1">
+                        <input
+                            className="input"
+                            type="text"
+                            placeholder="What you want to eat?"
+                        />
+                        <button className="btn">Search</button>
+                    </div>
+                    <div className="right-column-2">
+                        <div>Icon</div>
+                        <button className="btn secondary">
+                            Create Account
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
